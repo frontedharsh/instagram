a9c027810e51478f8a36478128a5c5eb7157b392
harshjain <pd.harshjain@gmail.com>
2024-09-14 22:41:37 +0530

    Initial commit

---
+++ insta.js
@@ -0,0 +1,11 @@
+var con=document.querySelector('.poster');
+var insta=document.getElementById('first');
+insta.style.display='';
+
+  
+var con=document.querySelector('.poster');
+
+var insta=document.getElementById('hide');
+con.addEventListener('dblclick',function(){
+  insta.style.display='block';
+})
